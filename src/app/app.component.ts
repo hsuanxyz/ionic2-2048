d340d0d61a48ae350242bed0903a257b82e57a0c
HsuanXyz <HsuanGM@gmail.com>
2017-01-08 13:58:40 +0800

    add start funtion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
-import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,7 +11,64 @@ export class HomePage {
   items:Array<number>;
 
   constructor(public navCtrl: NavController) {
-    this.items = [16384,8192,4096,2048,1024,512,256,128,64,32,16,8,4,2,0,0]
+    // this.items = [16384,8192,4096,2048,1024,512,256,128,64,32,16,8,4,2,0,0];
+    this.init();
   }
+
+  static getRandomIntInclusive(min:number, max:number):number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  init(){
+    this.items = new Array(16).fill(0);
+    this.randomFill();
+    this.randomFill();
+  }
+
+  start(){
+    this.init();
+  }
+
+  /**
+   * 获取元素为0的索引数组
+   * @returns {Array}
+   */
+  findFullIndex():Array<number>{
+    let indexArr = [];
+    this.items.forEach((item,index) => {
+      if(item ===0){
+        indexArr.push(index);
+      }
+    });
+    return indexArr;
+  }
+
+  /**
+   * 随机填充空位
+   */
+  randomFill() {
+    let indexArr: Array<number>;
+    let fillIndex: number;
+    let random: number;
+
+    indexArr = this.findFullIndex();
+    fillIndex = HomePage.getRandomIntInclusive(0,indexArr.length-1);
+    random = Math.random() >= 0.5 ? 4 : 2;
+    this.items[indexArr[fillIndex]] = random;
+  }
+
+  up(){
+    this.items.forEach( (item,index,arr) =>{
+      if((index-4) < 0) return;
+      if((arr[index-4]) === 0){
+        arr[index-4] = item;
+        arr[index] = 0;
+      }
+    })
+  }
+
+
 
 }
